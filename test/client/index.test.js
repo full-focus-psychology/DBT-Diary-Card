ae18b520909afb4c8f45251f8d3c9c013071c148
simonol1 <simondanolearyis@gmail.com>
2017-07-29 09:56:17 +1000

    adjusted title and tab, reviewing the boilerplate

--- test/client/index.test.js
+++ test/client/index.test.js
@@ -8,7 +8,7 @@ import './setup-dom'
 App.prototype.componentDidMount = () => {}
 
 
-test('Heading renders on App', t => {
-  const wrapper = shallow(<App />)
-  t.is(wrapper.find('h1').text(), 'Hello')
-})
+// test('Heading renders on App', t => {
+//   const wrapper = shallow(<App />)
+//   t.is(wrapper.find('h1').text(), 'Hello')
+// })
