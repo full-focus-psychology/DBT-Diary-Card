ad3007d01e9c21767c4789ca17d02670fddfb07b
simonol1 <simondanolearyis@gmail.com>
2017-08-08 11:56:56 +1000

    added server side routes for 2 tables

---
+++ server/routes/emotions.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var router = express.Router()
+
+var emotionsDb = require('../db/db')
+
+router.get('/', (req, res) => {
+  let db = req.app.get('db')
+  emotionsDb.getEmotionList(db)
+    .then(emotions => {
+      res.json(emotions)
+    })
+})
+
+
+
+module.exports = router
---
+++ server/routes/skills.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var router = express.Router()
+
+var skillsDb = require('../db/db')
+
+router.get('/', (req, res) => {
+  let db = req.app.get('db')
+  skillsDb.getSkillList(db)
+    .then(skills => {
+      res.json(skills)
+    })
+})
+
+
+
+module.exports = router
